247bca992de3239ebf728a165d5762169b2781a6
Peet Janse van Vuuren <peetjvv93@gmail.com>
2023-01-06 14:30:21 +0200

    setup workbox service worker to cache production builds in browser

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+/* eslint-disable no-undef */
+/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const WorkboxPlugin = require('workbox-webpack-plugin');
 
 module.exports = env => {
   const isDevServer = !!env.development;
@@ -61,7 +64,22 @@ module.exports = env => {
         filename: 'index.html',
         favicon: './src/favicon.ico',
       }),
-    ],
+      !isDevServer
+        ? new WorkboxPlugin.GenerateSW({
+            // See webpack guide to set up offline apps: https://webpack.js.org/guides/progressive-web-application/
+            // See list of available options: https://developer.chrome.com/docs/workbox/reference/workbox-build/#type-GenerateSWOptions
+            //
+            // These options encourage the ServiceWorkers to get in there fast
+            // and not allow any straggling "old" SWs to hang around.
+            clientsClaim: true,
+            skipWaiting: true,
+            // Increase maximum file size to cache so that we can cache dev build bundle.js files
+            maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3MB
+            // Set fallback so that loading subpages without navigating in the app will load that page. SPA stuff.
+            navigateFallback: '/index.html',
+          })
+        : null,
+    ].filter(plugin => !!plugin),
     devServer: {
       historyApiFallback: true,
     },
