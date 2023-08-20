c98a3a902fa82fab6b815b45992c16e8a759349e
Peet Janse van Vuuren <peetjvv93@gmail.com>
2023-08-20 21:22:09 +0200

    Fix serviceworker not registering by splitting webpack config file in order to not use the --env flag in the npm webpack scripts

---
+++ webpack.config.dev.ts
@@ -0,0 +1,25 @@
+import webpack from 'webpack';
+import WorkboxPlugin from 'workbox-webpack-plugin';
+import baseConfig from './webpack.config';
+
+const config: webpack.Configuration = {
+  ...baseConfig,
+  plugins: [
+    ...(baseConfig.plugins || []),
+    new WorkboxPlugin.GenerateSW({
+      // See webpack guide to set up offline apps: https://webpack.js.org/guides/progressive-web-application/
+      // See list of available options: https://developer.chrome.com/docs/workbox/reference/workbox-build/#type-GenerateSWOptions
+      //
+      // These options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around.
+      clientsClaim: true,
+      skipWaiting: true,
+      // Increase maximum file size to cache so that we can cache dev build bundle.js files
+      maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3MB
+      // Set fallback so that loading subpages without navigating in the app will load that page. SPA stuff.
+      navigateFallback: '/index.html',
+    }),
+  ],
+};
+
+export default config;
--- webpack.config.ts
+++ webpack.config.ts
@@ -1,86 +1,61 @@
 import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import WorkboxPlugin from 'workbox-webpack-plugin';
 import webpack from 'webpack';
 
-const configBuilder = (env: { development?: true }): webpack.Configuration => {
-  const isDevServer = !!env.development;
-
-  return {
-    entry: './src/index.tsx',
-    output: {
-      path: path.resolve(__dirname, 'build'),
-      filename: 'bundle.js',
-      publicPath: '/',
-    },
-    module: {
-      rules: [
-        {
-          test: /\.html$/,
-          loader: 'html-loader',
+const config: webpack.Configuration = {
+  entry: './src/index.tsx',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.(ts|tsx)$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: 'ts-loader',
         },
-        {
-          test: /\.(ts|tsx)$/,
-          exclude: /(node_modules)/,
-          use: {
-            loader: 'ts-loader',
-          },
-        },
-        {
-          test: /\.modernizrrc(\.json)?$/,
-          use: ['modernizr-loader', 'json-loader'],
-        },
-        {
-          test: /\.scss/,
-          use: [
-            require.resolve('style-loader'),
-            require.resolve('css-loader'),
-            {
-              loader: 'sass-loader',
-              options: {
-                implementation: require('sass'),
-              },
+      },
+      {
+        test: /\.modernizrrc(\.json)?$/,
+        use: ['modernizr-loader', 'json-loader'],
+      },
+      {
+        test: /\.scss/,
+        use: [
+          require.resolve('style-loader'),
+          require.resolve('css-loader'),
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass'),
             },
-          ],
-        },
-        {
-          test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        },
-      ],
-    },
-    resolve: {
-      extensions: ['.ts', '.tsx', '.js'],
-      // alias: {
-      //   settings: path.resolve(
-      //     isDevServer ? './src/settings.dev' : './src/settings.prod'
-      //   ),
-      // },
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: 'index.html',
-        favicon: './src/favicon.ico',
-      }),
-      !isDevServer
-        ? new WorkboxPlugin.GenerateSW({
-            // See webpack guide to set up offline apps: https://webpack.js.org/guides/progressive-web-application/
-            // See list of available options: https://developer.chrome.com/docs/workbox/reference/workbox-build/#type-GenerateSWOptions
-            //
-            // These options encourage the ServiceWorkers to get in there fast
-            // and not allow any straggling "old" SWs to hang around.
-            clientsClaim: true,
-            skipWaiting: true,
-            // Increase maximum file size to cache so that we can cache dev build bundle.js files
-            maximumFileSizeToCacheInBytes: 3 * 1024 * 1024, // 3MB
-            // Set fallback so that loading subpages without navigating in the app will load that page. SPA stuff.
-            navigateFallback: '/index.html',
-          })
-        : null,
-    ].filter(plugin => !!plugin),
-    devtool: 'source-map',
-  };
+          },
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      favicon: './src/favicon.ico',
+    }),
+  ],
+  devtool: 'source-map',
 };
 
-export default configBuilder;
+export default config;
